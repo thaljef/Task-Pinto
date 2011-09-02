211efe855aa62d08caf3a958075ab1d1240c9dc1
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2011-09-02 14:15:24 -0700

    Initial commit

---
+++ lib/Task/Pinto.pm
@@ -0,0 +1,35 @@
+package Task::Pinto;
+
+# ABSTRACT: All the Pinto components in one shot
+
+use strict;
+use warnings;
+
+#-------------------------------------------------------------------------------
+
+# VERSION
+
+#-------------------------------------------------------------------------------
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This Task installs all the Pinto components in one shot.  If you have
+created your own Pinto plugin, let me know so that I can add it here.
+
+=head1 CONTENTS
+
+=head2 L<Pinto>
+
+=head2 L<App::Pinto::Admin>
+
+=head2 L<Pinto::Remote>
+
+=head2 L<App::Pinto::Remote>
+
+=head2 L<Pinto::Server>
+
+=head2 L<Dist::Zilla::Plugin::ReleaseToPinto>
+
